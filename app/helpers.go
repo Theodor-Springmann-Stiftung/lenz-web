ef219a1c8455c7041f00d20bedf4fbfce04bf598
Simon Martens <simon.martens@mailbox.org>
2026-02-19 21:13:01 +0100

    FIX: helpers +tag inspection

--- app/helpers.go
+++ app/helpers.go
@@ -9,27 +9,6 @@ import (
 	"github.com/Theodor-Springmann-Stiftung/lenz-web/templates"
 )
 
-func (a *App) Error() error {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-	return a.err
-}
-
-func discoverPages(app *App) ([]Route, error) {
-	var routes []Route
-	for _, page := range RegisteredPages() {
-		discovered, err := page.Discover(app)
-		if err != nil {
-			return nil, err
-		}
-		for i := range discovered {
-			discovered[i].page = page
-		}
-		routes = append(routes, discovered...)
-	}
-	return routes, nil
-}
-
 func parseTemplates() (*template.Template, error) {
 	paths := make([]string, 0, 16)
 
